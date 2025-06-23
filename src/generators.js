cc8ad9674955eaeca2d1c169e788a730c86b03b3
KaterynaHl <glebakata50@gmail.com>
2025-06-24 00:31:21 +0300

    Fixed generators

--- src/generators.js
+++ src/generators.js
@@ -1,26 +1,9 @@
-function* colorGenerator() {
+export function* colorCycleGenerator() {
     const colors = ["red", "green", "blue", "yellow"];
-let index = 0;
-while (true) {
-    yield colors [index % colors.length];
-    index++;
-}
-}
-
-function consumeWithTimeout(iterator, timeoutSeconds = 3) {
-    const start = Date.now();
-
-    while (Date.now() - start < timeoutSeconds * 1000) {
-        const next = iterator.next();
-        if (next.done) break;
-        console.log ("Отримано: ", next.value)
+    let i = 0;
+    while (true) {
+      yield colors[i % colors.length];
+      i++;
     }
-
-    console.log ("Таймаут завершено.")
-}
-
-// експорт функцій, щоб використовувати в інших модулях
-module.exports = {
-    colorGenerator,
-    consumeWithTimeout,
-  };
+  }
+  
