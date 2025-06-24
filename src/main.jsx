3dfc79a53c5333450301cc1cf9ed281942ae0403
KaterynaHl <glebakata50@gmail.com>
2025-06-24 03:57:58 +0300

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SimonGame from './game.jsx';
+import Game from './game.jsx';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
